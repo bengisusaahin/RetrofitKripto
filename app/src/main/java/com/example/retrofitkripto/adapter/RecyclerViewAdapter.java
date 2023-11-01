9d4cf25268a8afd0e39259cd89798a8cafd97132
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-11-01 15:16:44 +0300

    Created colors adapter

---
+++ app/src/main/java/com/example/retrofitkripto/adapter/RecyclerViewAdapter.java
@@ -0,0 +1,59 @@
+package com.example.retrofitkripto.adapter;
+
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.retrofitkripto.R;
+import com.example.retrofitkripto.model.CryptoModel;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.RowHolder> {
+
+    private ArrayList<CryptoModel> cryptoList;
+    private String[] colors = {"#f38488","#18392b","#00a1de","#cb2c31","#93d0fc","#8b1ec4","#12af83","#86dcd5"};
+
+    public RecyclerViewAdapter(ArrayList<CryptoModel> cryptoList) {
+        this.cryptoList = cryptoList;
+    }
+
+    @NonNull
+    @Override
+    public RowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.row_layout, parent, false);
+        return new RowHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RowHolder holder, int position) {
+        holder.bind(cryptoList.get(position), colors, position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return cryptoList.size();
+    }
+
+    public class RowHolder extends RecyclerView.ViewHolder {
+        TextView textName;
+        TextView textPrice;
+        public RowHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        public void bind(CryptoModel cryptoModel, String[] colors, Integer position){
+            itemView.setBackgroundColor(Color.parseColor(colors[position % 8]));
+            textName = itemView.findViewById(R.id.text_name);
+            textPrice = itemView.findViewById(R.id.text_price);
+            textName.setText(cryptoModel.currency);
+            textPrice.setText(cryptoModel.price);
+        }
+    }
+}
